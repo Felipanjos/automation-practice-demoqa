226531f165431457618e03ec1a2e9dca1347abad
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2024-01-29 19:19:17 -0300

    feature and commands template

---
+++ cypress/e2e/check-box.feature
@@ -0,0 +1,29 @@
+Feature: Validate web element 
+  As user
+    I'm able to use and interact with the functionalities available at DemoQA's elements tab
+
+Background: Access Elements page
+  When I access "Elements" page
+
+Scenario: Access DemoQA website
+  When I access DemoQA's website
+    Then the image title "ToolsQA" will be displayed
+     
+  Scenario: Access Elements tab
+    When I access DemoQA's website
+      And I click "Elements" card
+        Then I'll be redirected to the "Elements" section of the page
+          And "Element" title will be displayed
+            And "Please select an item from left to start practice." text will be displayed
+              And "Elements" menu will be expanded
+
+  Scenario: Access Check Box tab
+    And I click "Check Box" submenu
+      Then I'll be redirected to Check Box page
+        And title will change to "Check Box"
+
+  Scenario: Expand "Home" toggle
+    And I click "Check Box" submenu
+      And I click the toggle arrow in "Home" label
+        Then the icon will change to an open folder
+          And 3 more expandable toogles will show up, Desktop, Documents and Downloads
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,42 +1,27 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 Cypress.Commands.addAll({
-  access() {
+  accessDemoQA() {
     cy.visit('https://demoqa.com');
     cy.get('header')
       .should('be.visible')
       .find('img').should('have.attr', 'src').should('include', 'Toolsqa');
   },
 
-  accessElements() {
+  accessElementsViaMenu() {
     cy.contains('h5', 'Elements')
       .should('be.visible')
       .click();
+    cy.url()
+      .should('include', 'elements');
     cy.contains('Please select an item from left to start practice.')
       .should('be.visible');
+    cy.get('div.element-list.collapse.show')
+      .should('be.visible');
+    cy.get('ul.menu-list')
+      .should('be.visible');
+  },
+
+  accessElementsDirectly() {
+    cy.accessDemoQA();
+    cy.accessElementsViaMenu();
   }
 });
